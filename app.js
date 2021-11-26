f53c7888f6a01b355b7ac7a5af2e68fe2e890266
Tanya Samal <solesytto@gmail.com>
2021-11-26 15:34:14 +0300

    refactor: change selectors using the BEM metodology according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ var createNewTaskElement = function (taskString) {
 
   //Each elements, needs appending
   checkBox.type = "checkbox";
+  checkBox.className = "checkbox";
   editInput.type = "text";
   editInput.className = "task-input";
 
@@ -87,7 +88,7 @@ var editTask = function () {
   var editInput = listItem.querySelector('.task-input');
   var label = listItem.querySelector("label");
   var editBtn = listItem.querySelector(".edit");
-  var containsClass = listItem.classList.contains("edit-mode");
+  var containsClass = listItem.classList.contains("todo-list__item_edit");
   //If class of the parent is .editmode
   if (containsClass) {
 
@@ -101,7 +102,7 @@ var editTask = function () {
   }
 
   //toggle .editmode on the parent.
-  listItem.classList.toggle("edit-mode");
+  listItem.classList.toggle("todo-list__item_edit");
 };
 
 
